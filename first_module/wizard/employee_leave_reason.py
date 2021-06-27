d2fb5b166004de5ffe0c80db7d54e445d937af39
Long <lqlongvn@gmail.com>
2021-06-27 21:01:23 +0700

    Print(employee_id), ok

--- first_module/wizard/employee_leave_reason.py
+++ first_module/wizard/employee_leave_reason.py
@@ -7,5 +7,7 @@ class EmployeeLeaveReason(models.TransientModel):
         selection=[('1', 'Nghi Thai San'), ('2', 'Nghi Phep'), ('3', 'Nghi Viec')], string='Reason')
 
     def update_leave_reason(self):
+        employee_id = self.env.context.get('active_id', False)
+        print(employee_id)
         return True
 
