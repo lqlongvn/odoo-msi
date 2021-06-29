1aa84cb5f41e3e5b8f96aa1989786363f35cdd21
Long <lqlongvn@gmail.com>
2021-06-29 12:49:52 +0700

    Hiển thị smart button trong customer_view.xml

--- bt1/models/customer.py
+++ bt1/models/customer.py
@@ -11,14 +11,15 @@ class Customer(models.Model):
 
     order_ids = fields.One2many(comodel_name='order', inverse_name='customer_id', string='Orders')
 
-    order_count = fields.Integer(compute='get_order_count', string='Count', store=True)
+    customer_order_count = fields.Integer(compute='get_order_count', string='Count', store=True)
 
     @api.depends('order_ids')
     def get_order_count(self):
         for customer in self:
             # print(order.order_ids[0].customer_id)
-            customer.order_count = 8
-            # order.customer_count = len(order.order_line_ids)
+            # customer.customer_order_count = 8
+            customer.customer_order_count = len(customer.order_ids)
+            # order.customer_order_count = len(order.order_line_ids)
 
     @api.model
     def create(self, vals):
