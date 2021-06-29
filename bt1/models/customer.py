31d91d839ca559d75703564359ae5e44ccd7bd59
Long <lqlongvn@gmail.com>
2021-06-29 08:33:03 +0700

    Tạo hàm get_order_count trong customer.py

--- bt1/models/customer.py
+++ bt1/models/customer.py
@@ -9,15 +9,15 @@ class Customer(models.Model):
     add = fields.Char(string='Address')
     gender = fields.Char(string='Giới tính', default="male")
 
-    # order_count = fields.Integer(compute='get_order_count', string='Count', store=True)
-
     order_ids = fields.One2many(comodel_name='order', inverse_name='customer_id', string='Orders')
 
-    # @api.depends('order_ids')
-    # def get_order_count(self):
-    #     for customer in self:
+    order_count = fields.Integer(compute='get_order_count', string='Count', store=True)
+
+    @api.depends('order_ids')
+    def get_order_count(self):
+        for customer in self:
             # print(order.order_ids[0].customer_id)
-            # customer.order_count = 8
+            customer.order_count = 8
             # order.customer_count = len(order.order_line_ids)
 
     @api.model
