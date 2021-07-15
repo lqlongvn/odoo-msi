e3375091fa83ad04d5e9752e0cec3689535566ec
Long <lqlongvn@gmail.com>
2021-07-15 21:34:09 +0700

    Phân quyền cho model wizard (count_order)

--- bt1/models/order.py
+++ bt1/models/order.py
@@ -32,15 +32,15 @@ class Order(models.Model):
             order.customer_count = len(order.order_line_ids)
 
     def cancel_order(self):
-        self.state = '0'
-        # elif self.state == '0':
-        #     self.state = '1'
-        # elif self.state == '1':
-        #     self.state = '2'
-        # elif self.state == '2':
-        #     self.state = '3'
-        # else:
-        #     self.state = '0'
+        # self.state = '0'
+        if self.state == '0':
+            self.state = '1'
+        elif self.state == '1':
+            self.state = '2'
+        elif self.state == '2':
+            self.state = '3'
+        else:
+            self.state = '0'
 
 
     def unlink(self):
