a2927fee01b043684e5facbb980170ca84f96d7b
Long <lqlongvn@gmail.com>
2021-07-31 22:16:52 +0700

    Hiển thị giá trị count_order_of_customer; Tạm thời, giá trị default là 5;

--- bt1/wizard/count_order_of_customer.py
+++ bt1/wizard/count_order_of_customer.py
@@ -3,4 +3,4 @@ from odoo import fields, models, api
 class EmployeeLeaveReason(models.TransientModel):
     _name = 'count.order'
 
-
+    count_order_of_customer = fields.Integer(string='Số lượng order của Customer', default=5)
