e606cd2a245159fa0ef472e6292f536bf7149584
Long <lqlongvn@gmail.com>
2021-08-01 18:16:20 +0700

    Tính toán giá trị count_order và truyền giá trị cho order_count_number_of_customer

--- bt1/wizard/count_order_of_customer.py
+++ bt1/wizard/count_order_of_customer.py
@@ -1,6 +1,12 @@
 from odoo import fields, models, api
 
-class EmployeeLeaveReason(models.TransientModel):
+class CountOrderOfCustomer(models.TransientModel):
     _name = 'count.order'
 
-    count_order_of_customer = fields.Integer(string='Số lượng order của Customer', default=5)
+    count_order_of_customer = fields.Integer(string='Số lượng order của Customer', default=0)
+
+    def get_order_count(self):
+        customer_id = self.env.context.get('active_id', False)
+        customer_record = self.env['customer'].browse(customer_id)
+        count_order_of_customer= customer_record.customer_order_count
+        return count_order_of_customer
