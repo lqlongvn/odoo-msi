6e50ae9b65bfe37790d14c423315e1a4ae1ab650
Long <lqlongvn@gmail.com>
2021-08-07 09:19:28 +0700

    Đã hiển thị giá trị biến customer_total_money trên wizard

--- thi/wizard/customer_total_money.py
+++ thi/wizard/customer_total_money.py
@@ -3,5 +3,5 @@ from odoo import fields, models, api
 class Customer_Total_Money(models.TransientModel):
     _name = 'customertotalmoney'
 
-    customer_total_money = fields.Integer(string='Tổng số tiền của các đơn hàng của Customer', default=100)
+    customer_total_money = fields.Integer(string='Tổng số tiền của các đơn hàng của Customer', default='100')
 
