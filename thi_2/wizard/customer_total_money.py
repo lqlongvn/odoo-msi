1d37e994e3e666e344c3059bbbe58846a75697c8
Long <lqlongvn@gmail.com>
2021-08-14 17:35:01 +0700

    Đã hiển thị wizard (lần 3); Bổ sung default cho biến "customer_total_money"

--- thi_2/wizard/customer_total_money.py
+++ thi_2/wizard/customer_total_money.py
@@ -4,4 +4,4 @@ class Customer_Total_Money(models.TransientModel):
     _name = 'customertotalmoney2'
 
     # customer_total_money = fields.Integer(string='Tổng số tiền của các đơn hàng của Customer', default='100')
-    customer_total_money = fields.Integer(string='Tổng số tiền của các đơn hàng của Customer')
+    customer_total_money = fields.Integer(string='Tổng số tiền của các đơn hàng của Customer', default=100)
