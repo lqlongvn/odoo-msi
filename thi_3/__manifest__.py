4c8ae318b879a374a5951237f0df233016766621
Long <lqlongvn@gmail.com>
2021-08-19 21:44:12 +0700

    Đã khởi tạo xong Project Thi-3 từ đầu; ok;

---
+++ thi_3/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'thi_3',
+    'version': '1.0',
+    'summary': 'Thi 3 project summary',
+    'sequence': 1,
+    'author': 'Long (Thi 3)',
+    'description': """
+        Thi project description
+    """,
+    'category': 'Other',
+    'website': '',
+    'depends': ['sales_team'],
+    'data': [
+        'views/customer_view.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': True,
+}
