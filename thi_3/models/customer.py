7fc70e6a5024aa464964ed7e659c56ec6bf3d51e
Long <lqlongvn@gmail.com>
2021-08-20 08:44:37 +0700

    Uninstall project thi_3 và sửa model customer3 -> model khachhang

--- thi_3/models/customer.py
+++ thi_3/models/customer.py
@@ -3,7 +3,7 @@ from odoo import fields, models, api
 # customer (id, name, phone, address, gender - gan gender mac dinh la male)
 
 class Customer(models.Model):
-    _name = 'customer3'
+    _name = 'khachhang'
 
     name = fields.Char(string='Tên khách hàng')
     phone = fields.Char(string='Điện thoại')
