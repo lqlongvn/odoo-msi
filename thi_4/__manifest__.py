2c093efde16c1af34cc60c9e27d255fb20ba8ea9
lqlongvn <lqlongvn@gmail.com>
2021-09-28 09:30:58 +0700

    thi_4, ôn lần cuối trước khi thi

---
+++ thi_4/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'thi_4 (ôn lần cuối)',
+    'version': '1.0',
+    'summary': 'Thi 4 project summary',
+    'sequence': 1,
+    'author': 'Long (Thi 4)',
+    'description': """
+        Thi project description
+    """,
+    'category': 'Other',
+    'website': '',
+    'depends': [],
+    'data': [
+        'views/customer_view.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': True,
+}
